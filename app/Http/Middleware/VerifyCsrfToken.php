bc25812a1b71ff7c61235341a0c31c8f2a8136be
Nithichai <rockon23146@gmail.com>
2019-02-22 03:30:07 +0700

    Settting

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -19,6 +19,10 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        '/userid/*'
+        '/user-line/*',
+        '/device/*',
+        '/device-info/*',
+        '/user-info/*',
+        '/logout'
     ];
 }
--- database/factories/DeviceFactory.php
+++ database/factories/DeviceFactory.php
@@ -1,11 +1,12 @@
 <?php
 
 use Faker\Generator as Faker;
+use Illuminate\Support\Facades\Hash;
 
 $factory->define(App\Device::class, function (Faker $faker) {
     return [
         'espname' => "",
-        'password' => "Smarthealper",
+        'password' => Hash::make("Smarthealper"),
         'deviceid' => ""
     ];
 });
