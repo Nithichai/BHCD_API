238c5add7e1e35664a06058979d19c8fd5aee1c3
Nithichai <rockon23146@gmail.com>
2019-02-22 03:28:37 +0700

    New Model's Name

---
+++ app/DeviceInfo.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DeviceInfo extends Model
+{
+    protected $table = 'device_infomation';
+    
+    protected $primaryKey = 'id';
+
+}
