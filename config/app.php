b919b61bca80534bf879664fa9e64a6016bf06c8
Nithichai <rockon23146@gmail.com>
2019-02-25 14:19:43 +0700

    Nerver Give Up

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -119,7 +119,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY', 'base64:ta/gpJLszHskuTav34AO5+evSrsiWYVKDSVfZ4xpai8='),
 
     'cipher' => 'AES-256-CBC',
 
